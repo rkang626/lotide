5349e65e36101d52ce13a31616ab1d850f0f8ae3
Ryan Kang <rkang626@gmail.com>
2021-01-04 21:58:33 +0000

    use template strings

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  console.log(actual === expected ? '🔥🔥🔥 Assertion Passed: ' + actual + ' === ' + expected : '💦💦💦 Assertion Failed: ' + actual + ' !== ' + expected);
+  console.log(actual === expected ? `🔥🔥🔥 Assertion Passed: ${actual} === ${expected}` : `💦💦💦 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // TEST CODE
