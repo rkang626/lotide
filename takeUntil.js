0a940482442c33678c7b21c97423c3f45eeaa194
Ryan Kang <rkang626@gmail.com>
2021-01-07 22:39:25 +0000

    add takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+
+  let equal = true;
+  if (arrayOne.length !== arrayTwo.length) {
+    equal = false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        equal = false;
+      }
+    }
+  }
+  
+  return equal;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  console.log(eqArrays(actual, expected) ? `🔥🔥🔥 Assertion Passed: ${actual} === ${expected}` : `💦💦💦 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const takeUntil = function(array, callback) {
+  let output = [];
+
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    } else {
+      output.push(item);
+    }
+  }
+
+  return output;
+};
+
+// test cases
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+assertArraysEqual(takeUntil(data2, x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
