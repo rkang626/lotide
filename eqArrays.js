938a123c94bde3feb9707ccc00d06a8c7141beb0
Ryan Kang <rkang626@gmail.com>
2021-01-08 23:45:03 +0000

    update eqArrays to handle nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -3,27 +3,32 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arrayOne, arrayTwo) {
-  let equal = true;
 
   if (!Array.isArray(arrayOne) || !Array.isArray(arrayTwo)) {
     return false;
   }
 
   if (arrayOne.length !== arrayTwo.length) {
-    equal = false;
+    return false;
   } else {
     for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        equal = false;
+      if (Array.isArray(arrayOne[i])) {
+        if (!eqArrays(arrayOne[i], arrayTwo[i])) {
+          return false;
+        }
+      } else if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
       }
     }
   }
 
-  return equal;
+  return true;
 };
 
 // test cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // => should NOT PASS
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); // => should NOT PASS
-assertEqual(eqArrays([1, 2, 3], []), false); // => should NOT PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // => should NOT PASS
+// assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); // => should NOT PASS
+// assertEqual(eqArrays([1, 2, 3], []), false); // => should NOT PASS
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => should PASS
