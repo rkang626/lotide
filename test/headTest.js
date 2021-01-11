5079412c1ad0c708c32ec544284a453d53f59900
Ryan Kang <rkang626@gmail.com>
2021-01-11 16:09:46 +0000

    remove old test code

--- test/headTest.js
+++ test/headTest.js
@@ -12,9 +12,3 @@ describe("#head", () => {
   });
 
 });
-
-// test cases
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([5]), 5);
-// assertEqual(head([]), undefined);
--- test/middleTest.js
+++ test/middleTest.js
@@ -20,13 +20,3 @@ describe("#middle", () => {
   });
 
 });
-
-// test cases
-// assertArraysEqual(middle([1]), []);
-// assertArraysEqual(middle([1, 2]), []);
-
-// assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
